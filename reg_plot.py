6a2ffce8e40dc1a114cd3272149ec284312c1afe
AlcoholicDragon <devakupadhyay@gmail.com>
2020-11-17 13:29:31 +0530

    hey jude

--- reg_plot.py
+++ reg_plot.py
@@ -10,6 +10,6 @@ rai = pd.read_csv("CSV_files/first/rainYearly.csv")
 
 tem["Rainfall"]=rai["Rainfall"]
 
-sns.regplot(x="Rainfall", y="Temperature", data=tem)
-    
+sns.regplot(x="Rainfall",y="Temperature",data=tem[(0.0 <= tem['Rainfall']) & (tem['Rainfall'] < 10.0) & (0.0 <= tem['Temperature']) & (tem['Temperature'] < 50.0)])    
+
 show()
