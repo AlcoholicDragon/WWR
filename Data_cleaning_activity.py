22f533fa812fa8eb0016c4beeb90cfd8e37eda80
AlcoholicDragon <devakupadhyay@gmail.com>
2020-11-17 21:58:16 +0530

    just do it

--- Data_cleaning_activity.py
+++ Data_cleaning_activity.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 from matplotlib.pyplot import *
 import numpy as np
+from random import randint
 
 sns.set_theme()
 
@@ -15,7 +16,20 @@ med_t = tem["Temperature"].median()
 
 med_r = tem["Rainfall"].median()
 
+#checking the data type of all entries
+for i in tem:
+    for j in range(len(tem[i])):
+        type(tem[i][j])
+
+#changing two random float data type entries to int
+for i in range(2):
+    k=randint(1,61)
+    int(tem["Rainfall"][k])
+
+#changing Null values (Nan) to median
+
 cr=0
+
 for i in tem["Rainfall"]:
     try:
         int(i)
@@ -24,12 +38,15 @@ for i in tem["Rainfall"]:
     cr+=1
 
 ct=0
+
 for j in tem["Temperature"]:
     try:
         int(j)
     except ValueError:
         tem.loc[ct,"Temperature"]=med_t
     ct+=1
+
+#plotting the graph
 
 sns.regplot(x="Rainfall",y="Temperature",data=tem[(0.0 <= tem['Rainfall']) & (tem['Rainfall'] < 10.0) & (0.0 <= tem['Temperature']) & (tem['Temperature'] < 50.0)]) 
 
